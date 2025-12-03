2465cd15c17f73b95d9ad9eca0edb41e06d05498
tanvirp10 <tanvirp10@gmail.com>
2025-12-02 20:40:00 -0500

    Use environment variables for production database

--- config/config.js
+++ config/config.js
@@ -15,10 +15,11 @@ export default {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: 'tpatwary',
-    database: 'coffeeapp_production',
-    host: '127.0.0.1',
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'railway',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
   },
 };
